5bcf22c4d6300848233be6bb70b5820a8614cc52
forusu <27868690+forusu@users.noreply.github.com>
2026-02-05 14:45:14 +0100

    Added breaking news, weather reports, fixed things

--- scripts/index.js
+++ scripts/index.js
@@ -77,4 +77,55 @@ document.addEventListener("DOMContentLoaded", () => {
     scheduleBlink();
 });
 
-// Timed backgrounds
+// Breaking news:
+async function loadBreakingNews() {
+  try {
+    const res = await fetch('../data/news.txt', { cache: 'no-store' });
+    const text = await res.text();
+    const empty = "THERE ARE NO BREAKING NEWS AT THIS TIME"
+
+    if (!text) {
+      document.getElementById('news').innerHTML = empty.trim();
+    } else {
+      document.getElementById('news').innerHTML = text.trim().toUpperCase();
+    }
+    
+  } catch (err) {
+    console.log('UNABLE TO FETCH NEWS')
+  }
+}
+
+loadBreakingNews();
+
+// Weather stuff:
+async function loadWeather() {
+  try {
+    const res = await fetch('https://wttr.in/?format=%l|%t|%f|%C|%c|%p', {
+      cache: 'no-store'
+    });
+
+    const text = (await res.text()).trim();
+    const empty = "NO WEATHER INFORMATION COULD BE FOUND"
+
+    const [area, temp, feels, condition, emoji, precip] = text.split('|');
+
+    let output = `${area.toUpperCase()} - T:${temp} / F:${feels} | ${emoji} ${condition.toUpperCase()}`;
+
+    if (precip && precip !== '0mm') {
+      output += ` | PRECIPITATION: ${precip}`;
+    }
+
+    if (!text) {
+      document.getElementById('weather').textContent = empty;
+    } else {
+      document.getElementById('weather').textContent = output;
+    }
+
+  } catch (err) {
+    console.error('Weather fetch failed:', err);
+    document.getElementById('weather').textContent = empty;
+  }
+}
+
+loadWeather();
+setInterval(loadWeather, 10 * 60 * 1000);
